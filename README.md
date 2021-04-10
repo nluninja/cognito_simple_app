8c13b7713c32f86b013a6b6e207ec1e434293a88
nluninja <andrea.belli@gmail.com>
2021-04-10 18:50:00 +0200

    added AWS SDK dependency

---
+++ README.md
@@ -0,0 +1,25 @@
+# A simple Amazon Cognito Client
+
+Amazon Cognito is Amazon Web Services’ service for managing user authentication and access control.
+
+Getting Started
+Sign up for AWS
+Before you begin, you need an AWS account. Please see the Sign Up for AWS section of the developer guide for information about how to create an AWS account and retrieve your AWS credentials.
+
+Minimum requirements
+To run the SDK you will need Java 1.8+. For more information about the requirements and optimum settings for the SDK, please see the Installing a Java Development Environment section of the developer guide.
+
+
+## todo  
+switch to single repo 
+
+<dependency>
+  <groupId>software.amazon.awssdk</groupId>
+  <artifactId>ec2</artifactId>
+  <version>2.16.39</version>
+</dependency>
+<dependency>
+  <groupId>software.amazon.awssdk</groupId>
+  <artifactId>s3</artifactId>
+  <version>2.16.39</version>
+</dependency>
---
+++ src/main/java/nluninja/aws/cognito/CognitoClient.java
@@ -0,0 +1,96 @@
+package nluninja.aws.cognito;
+
+import org.json.JSONObject;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.Scanner;
+
+
+public class CognitoClient {
+
+    private String POOL_ID;
+    private String CLIENTAPP_ID;
+    private String CLIENT_SECRET;
+    private String FED_POOL_ID;
+    private String CUSTOMDOMAIN;
+    private String REGION;
+
+    final public static String REDIRECT_URL = "https://sid343.reinvent-workshop.com";
+
+    public CognitoClient() {
+
+        Properties prop = new Properties();
+        InputStream input = null;
+
+        try {
+            input = getClass().getClassLoader().getResourceAsStream("config.properties");
+
+            // load a properties file
+            prop.load(input);
+
+            // Read the property values
+            POOL_ID = prop.getProperty("POOL_ID");
+            CLIENTAPP_ID = prop.getProperty("CLIENTAPP_ID");
+            CLIENT_SECRET = prop.getProperty("CLIENT_SECRET");
+            FED_POOL_ID = prop.getProperty("FED_POOL_ID");
+            CUSTOMDOMAIN = prop.getProperty("CUSTOMDOMAIN");
+            REGION = prop.getProperty("REGION");
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    String GetHostedSignInURL() {
+        String customurl = "https://%s.auth.%s.amazoncognito.com/login?response_type=code&client_id=%s&redirect_uri=%s";
+
+        return String.format(customurl, CUSTOMDOMAIN, REGION, CLIENTAPP_ID, CognitoClient.REDIRECT_URL);
+    }
+
+
+     public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+
+         CognitoClient client = new CognitoClient();
+
+         System.out.println("Cognito Sample Login App");
+
+         System.out.println("Please enter the username: ");
+         String username = scanner.nextLine();
+
+         System.out.println("Please enter the password: ");
+         String password = scanner.nextLine();
+
+         String result = client.validate(username, password);
+         if (result != null) {
+             System.out.println("User is authenticated: " + result);
+         } else {
+             System.out.println("Username/password is invalid.");
+         }
+
+         JSONObject payload = CognitoJWTParser.getPayload(result);
+     //    String provider = payload.get("iss").toString().replace("https://", "");
+
+        // Credentials credentials = helper.GetCredentials(provider, result);
+       //  ListBuckets(credentials);
+
+     }
+
+    String validate(String username, String password) {
+        AuthenticationManager helper = new AuthenticationManager(POOL_ID, CLIENTAPP_ID, CLIENT_SECRET);
+        return helper.PerformSRPAuthentication(username, password);
+    }
+
+    }
+
+
+}
